2999985f27f84265f2eff7ab4a9dfe51bb2bef2d
ZLindl <zander.lindl@gmail.com>
2025-04-18 21:24:54 +0100

    New FIles

---
+++ consoleBank.js
@@ -0,0 +1,332 @@
+export const leBank = [
+  {
+    "name": "Magnavox Odyssey",
+    "year": 1972,
+    "manufacturer": "Magnavox",
+    "image": "Console Images/Magnavox Odyssey.jpg"
+  },
+  {
+    "name": "Atari Pong",
+    "year": 1975,
+    "manufacturer": "Atari",
+    "image": "Console Images/Atari Pong.png"
+  },
+  {
+    "name": "Coleco Telestar",
+    "year": 1976,
+    "manufacturer": "Coleco",
+    "image": "Console Images/Coleco Telestar.jpg"
+  },
+  {
+    "name": "Atari 2600",
+    "year": 1977,
+    "manufacturer": "Atari",
+    "image": "Console Images/Atari 2600.jpg"
+  },
+  {
+    "name": "Magnavox Odyssey 2",
+    "year": 1978,
+    "manufacturer": "Magnavox",
+    "image": "Console Images/Magnavox Odyssey 2.jpg"
+  },
+  {
+    "name": "Mattel Intellivision",
+    "year": 1979,
+    "manufacturer": "Mattel",
+    "image": "Console Images/Mattel Intellivision.png"
+  },
+  {
+    "name": "ColecoVision",
+    "year": 1982,
+    "manufacturer": "Coleco",
+    "image": "Console Images/ColecoVision.jpg"
+  },
+  {
+    "name": "Atari 5200",
+    "year": 1982,
+    "manufacturer": "Atari",
+    "image": "Console Images/Atari 5200.jpg"
+  },
+  {
+    "name": "Vectrex",
+    "year": 1982,
+    "manufacturer": "GCE",
+    "image": "Console Images/Vectrex.jpg"
+  },
+  {
+    "name": "NES",
+    "year": 1983,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/NES.png"
+  },
+  {
+    "name": "Sega SG 1000",
+    "year": 1983,
+    "manufacturer": "Sega",
+    "image": "Console Images/Sega SG 1000.jpg"
+  },
+  {
+    "name": "Sega Master System",
+    "year": 1985,
+    "manufacturer": "Sega",
+    "image": "Console Images/Sega Master System.png"
+  },
+  {
+    "name": "Atari 7800",
+    "year": 1986,
+    "manufacturer": "Atari",
+    "image": "Console Images/Atari 7800.jpg"
+  },
+  {
+    "name": "TurboGrafx 16",
+    "year": 1987,
+    "manufacturer": "NEC",
+    "image": "Console Images/TurboGrafx 16.jpg"
+  },
+  {
+    "name": "Sega Mega Drive",
+    "year": 1988,
+    "manufacturer": "Sega",
+    "image": "Console Images/Sega Mega Drive.jpg"
+  },
+  {
+    "name": "SNES",
+    "year": 1990,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/SNES.jpg"
+  },
+  {
+    "name": "Neo Geo AES",
+    "year": 1990,
+    "manufacturer": "SNK",
+    "image": "Console Images/Neo Geo AES.png"
+  },
+  {
+    "name": "Panasonic 3DO",
+    "year": 1993,
+    "manufacturer": "Panasonic",
+    "image": "Console Images/Panasonic 3DO.jpg"
+  },
+  {
+    "name": "Atari Jaguar",
+    "year": 1993,
+    "manufacturer": "Atari",
+    "image": "Console Images/Atari Jaguar.jpg"
+  },
+  {
+    "name": "Sony Playstation",
+    "year": 1994,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony Playstation.jpg"
+  },
+  {
+    "name": "Sega Saturn",
+    "year": 1994,
+    "manufacturer": "Sega",
+    "image": "Console Images/Sega Saturn.png"
+  },
+  {
+    "name": "Nintendo 64",
+    "year": 1996,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo 64.jpg"
+  },
+  {
+    "name": "Sega Dreamcast",
+    "year": 1998,
+    "manufacturer": "Sega",
+    "image": "Console Images/Sega Dreamcast.jpg"
+  },
+  {
+    "name": "Sony Playstation 2",
+    "year": 2000,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony Playstation 2.jpg"
+  },
+  {
+    "name": "Microsoft Xbox",
+    "year": 2001,
+    "manufacturer": "Microsoft",
+    "image": "Console Images/Microsoft Xbox.png"
+  },
+  {
+    "name": "Nintendo Gamecube",
+    "year": 2001,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Gamecube.jpg"
+  },
+  {
+    "name": "Microsoft Xbox 360",
+    "year": 2005,
+    "manufacturer": "Microsoft",
+    "image": "Console Images/Microsoft Xbox 360.jpg"
+  },
+  {
+    "name": "Sony Playstation 3",
+    "year": 2006,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony Playstation 3.jpg"
+  },
+  {
+    "name": "Nintendo WII",
+    "year": 2006,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo WII.png"
+  },
+  {
+    "name": "Nintendo WII U",
+    "year": 2012,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo WII U.png"
+  },
+  {
+    "name": "Sony Playstation 4",
+    "year": 2013,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony Playstation 4.jpg"
+  },
+  {
+    "name": "Microsoft Xbox One",
+    "year": 2013,
+    "manufacturer": "Microsoft",
+    "image": "Console Images/Microsoft Xbox One.png"
+  },
+  {
+    "name": "Sony Playstation 5",
+    "year": 2020,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony Playstation 5.jpg"
+  },
+  {
+    "name": "Microsoft Xbox Series X and Series S",
+    "year": 2020,
+    "manufacturer": "Microsoft",
+    "image": "Console Images/Microsoft Xbox Series X and Series S.png"
+  },
+  {
+    "name": "Atari Lynx",
+    "year": 1989,
+    "manufacturer": "Atari",
+    "image": "Console Images/Atari Lynx.png"
+  },
+  {
+    "name": "Nintendo Game Boy",
+    "year": 1989,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Game Boy.jpg"
+  },
+  {
+    "name": "Sega Game Gear",
+    "year": 1990,
+    "manufacturer": "Sega",
+    "image": "Console Images/Sega Game Gear.png"
+  },
+  {
+    "name": "Nintendo Game Boy Colour",
+    "year": 1998,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Game Boy Colour.jpg"
+  },
+  {
+    "name": "Neo Geo Pocket",
+    "year": 1998,
+    "manufacturer": "SNK",
+    "image": "Console Images/Neo Geo Pocket.jpg"
+  },
+  {
+    "name": "Nintendo Gameboy Advance",
+    "year": 2001,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Gameboy Advance.png"
+  },
+  {
+    "name": "Nintendo Gameboy Advance SP",
+    "year": 2003,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Gameboy Advance SP.png"
+  },
+  {
+    "name": "Nokia N-Gage",
+    "year": 2003,
+    "manufacturer": "Nokia",
+    "image": "Console Images/Nokia N-Gage.jpg"
+  },
+  {
+    "name": "Nintendo DS",
+    "year": 2004,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo DS.png"
+  },
+  {
+    "name": "Sony PlayStation Portable",
+    "year": 2004,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony PlayStation Portable.png"
+  },
+  {
+    "name": "Nintendo DS Lite",
+    "year": 2006,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo DS Lite.jpg"
+  },
+  {
+    "name": "Nintendo DSI",
+    "year": 2008,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo DSI.jpg"
+  },
+  {
+    "name": "Sony PlayStation Vita",
+    "year": 2011,
+    "manufacturer": "Sony",
+    "image": "Console Images/Sony PlayStation Vita.png"
+  },
+  {
+    "name": "Nintendo 3DS",
+    "year": 2011,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo 3DS.png"
+  },
+  {
+    "name": "Nintendo 2DS",
+    "year": 2013,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo 2DS.jpg"
+  },
+  {
+    "name": "New Nintendo 3DS",
+    "year": 2015,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/New Nintendo 3DS.png"
+  },
+  {
+    "name": "New Nintendo 2DS XL",
+    "year": 2017,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/New Nintendo 2DS XL.jpg"
+  },
+  {
+    "name": "Nintendo Switch",
+    "year": 2017,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Switch.jpg"
+  },
+  {
+    "name": "Nintendo Switch Lite",
+    "year": 2019,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Switch Lite.png"
+  },
+  {
+    "name": "Steam Deck",
+    "year": 2022,
+    "manufacturer": "Valve",
+    "image": "Console Images/Steam Deck.jpg"
+  },
+  {
+    "name": "Nintendo Switch 2",
+    "year": 2025,
+    "manufacturer": "Nintendo",
+    "image": "Console Images/Nintendo Switch 2.png"
+  }
+];
